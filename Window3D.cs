618664ce7c950a5990d7d1f7b69c4867315a95b9
wog <ovidiug@usm.ro>
2024-11-13 10:56:29 +0200

    Add project files.

---
+++ Window3D.cs
@@ -0,0 +1,86 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using System;
+using System.Drawing;
+
+namespace OpenTK3_CLI_template {
+    internal class Window3D : GameWindow {
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+
+        private readonly Axes ax;
+        private Camera cam;
+
+        private readonly Color DEFAULT_BKG_COLOR = Color.FromArgb(49, 50, 51);
+
+        public Window3D() : base(1280, 768, new GraphicsMode(32, 24, 0, 8)) {
+            VSync = VSyncMode.On;
+
+            // inits
+            ax = new Axes();
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+                       
+        }
+
+        protected override void OnResize(EventArgs e) {
+            base.OnResize(e);
+
+            // set background
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+
+            // set viewport
+            GL.Viewport(0, 0, this.Width, this.Height);
+
+            // set perspective
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 1024);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+
+            // set the eye
+            cam = new Camera();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e) {
+            base.OnUpdateFrame(e);
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape]) {
+                Exit();
+            }
+
+            previousKeyboard = currentKeyboard;
+            previousMouse = currentMouse;
+            // END logic code
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e) {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER CODE
+            ax.Draw();
+
+
+            // END render code
+
+            SwapBuffers();
+        }
+
+    }
+}
